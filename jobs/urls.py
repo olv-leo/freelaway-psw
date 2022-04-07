bf321a88dc82b2f29f0299d45f4068c6584fffbe
olv-leo <1996olv.leo@gmail.com>
2022-04-07 11:49:55 -0300

    Criação da área de jobs e geração automática dos cards de Job com base nos Jobs cadastrados no BD

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.)
+    path('encontrar_jobs/', views.encontrar_jobs, name="encontrar_jobs")
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Jobs
 
-# Create your views here.
+def encontrar_jobs(request):
+    if request.method == "GET":
+        jobs = Jobs.objects.filter(reservado=False)
+        return render(request, 'encontrar_jobs.html', {'jobs': jobs})
