ac74f65cb3051dec24e2d157a1aa9cfea7955f1d
olv-leo <1996olv.leo@gmail.com>
2022-04-07 11:11:42 -0300

    Criação arquivos base do Heroku e mudança para o pipenv

--- freelaway/settings.py
+++ freelaway/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h%gkn$zwz#piy20b-c@f*xy-^7r#_+s+^opn*z=ib4a)_s(4s*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'autenticacao',
+    'jobs',
 ]
 
 MIDDLEWARE = [
---
+++ jobs/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Referencias, Jobs
+
+admin.site.register(Referencias)
+admin.site.register(Jobs)
