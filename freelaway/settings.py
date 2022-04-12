94b612f464a98d6ca0e4582ed066e891a0ae67c4
olv-leo <1996olv.leo@gmail.com>
2022-04-12 15:19:15 -0300

    Ajuste na variável MEDIA_URL

--- freelaway/settings.py
+++ freelaway/settings.py
@@ -117,7 +117,7 @@ STATICFILES_DIRS = (BASE_DIR / 'templates/static',)
 STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/media/'
+MEDIA_URL = './media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
